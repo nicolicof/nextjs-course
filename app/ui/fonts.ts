f3e2410d929bd86e46e22f1fc944566f843e5a14
nicolicof <nicolicof6060@gmail.com>
2025-05-13 01:13:07 -0300

    Uploading files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ['latin'] })
+
+// Lusitana(options: { weight: "400" | "700" | Array<"400" | "700">; style?: "normal" | Array<"normal">; display?: Display; variable?: undefined; preload?: boolean; fallback?: string[]; adjustFontFallback?: boolean; subsets?: Array<"latin">; }): NextFont
